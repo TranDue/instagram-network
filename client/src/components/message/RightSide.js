f57ab6808c761cf4c44bd860a9b0fbc16afbbda8
Tran Due <tran.congdue1999@gmail.com>
2021-06-21 23:30:32 +0700

    make it better

--- client/src/components/message/RightSide.js
+++ client/src/components/message/RightSide.js
@@ -58,8 +58,8 @@ const RightSide = () => {
         files.forEach(file => {
             if (!file) return err = "File does not exist."
 
-            if (file.size > 1024 * 1024 * 5) {
-                return err = "The image/video largest is 5mb."
+            if (file.size > 1024 * 1024 * 10) {
+                return err = "The image/video largest is 10mb."
             }
 
             return newMedia.push(file)
