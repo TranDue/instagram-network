36c5fd01259e0266f6dffad2ecad7230125f9b6c
Tran Due <tran.congdue1999@gmail.com>
2021-06-23 22:29:27 +0700

    deploy

--- client/src/utils/imageUpload.js
+++ client/src/utils/imageUpload.js
@@ -23,10 +23,10 @@ export const imageUpload = async (images) => {
             formData.append("file", item)
         }
 
-        formData.append("upload_preset", "efxjficn")
-        formData.append("cloud_name", "devat-channel")
+        formData.append("upload_preset", "cye4ewmq")
+        formData.append("cloud_name", "due")
 
-        const res = await fetch("https://api.cloudinary.com/v1_1/devat-channel/upload", {
+        const res = await fetch("https://api.cloudinary.com/v1_1/due/upload", {
             method: "POST",
             body: formData
         })
