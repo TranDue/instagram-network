07f9b2b5aa88d85a2277bad20a2515c9efbb367f
Tran Due <tran.congdue1999@gmail.com>
2021-07-02 18:35:25 +0700

    update bo cuc

--- client/src/components/NotifyModal.js
+++ client/src/components/NotifyModal.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import NoNotice from '../images/notify.jpg'
 import { Link } from 'react-router-dom'
 import Avatar from './Avatar'
 import moment from 'moment'
